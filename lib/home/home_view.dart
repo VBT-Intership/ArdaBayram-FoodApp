38160bcd693e780483b00a8d7a562485b47ad3ce
Arda Bayram <56687124+arda-bayram@users.noreply.github.com>
2020-09-02 11:08:54 +0300

    Complete popular list and exract methods

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -9,129 +9,138 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    List<String> categoryList = List.generate(10, (index) => index.toString());
+    List<String> foodList = List.generate(10, (index) => index.toString());
     return Scaffold(
       appBar: buildAppBar(),
-      body: SafeArea(
-        child: Column(
-          children: [
-            buildSearchRow(),
-            buildColumnProduct(categoryList),
-            buildPopularRow(),
-            SizedBox(
-              height: 200,
-              child: ListView.builder(
-                itemCount: categoryList.length,
-                scrollDirection: Axis.vertical,
-                itemBuilder: (context, index) {
-                  return Container(
-                    child: Card(
-                      child: Row(
-                        children: <Widget>[
-                          Container(
-                            height: 100,
-                            width: 100,
-                            padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20)),
-                            child:
-                                Image.network("https://picsum.photos/200/200"),
-                          ),
-                          Container(
-                            padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      "Banana Goreng",
-                                      style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold),
-                                    )
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    SizedBox(
-                                        height: 18,
-                                        width: 60,
-                                        child: Center(
-                                          child: Container(
-                                            child:
-                                                Center(child: Text("Gorengan")),
-                                            color: Colors.orangeAccent,
-                                          ),
-                                        )),
-                                    SizedBox(
-                                        height: 18,
-                                        width: 150,
-                                        child: Center(
-                                          child: Container(
-                                            padding:
-                                                EdgeInsets.fromLTRB(5, 0, 0, 0),
-                                            child: Center(
-                                                child: Text("Makanan Ringan")),
-                                            color: Colors.blueAccent,
-                                          ),
-                                        )),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Container(
-                                      child:
-                                          Center(child: Text("Warung buyakuk")),
-                                    ),
-                                    // Spacer(),
-                                    Container(
-                                      padding:
-                                          EdgeInsets.fromLTRB(100, 0, 5, 0),
-                                      child: Center(
-                                          child: Text(
-                                        "Rp. 2.500",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold),
-                                      )),
-                                    ),
-                                    // SizedBox(
-                                    //   height: 18,
-                                    //   width: 60,
-                                    //   child: Container(
-                                    //     child: Center(
-                                    //         child: Text("Warung buyakuk")),
-                                    //   ),
-                                    // ),
-                                    // Spacer(),
-                                    // SizedBox(
-                                    //     height: 18,
-                                    //     width: 150,
-                                    //     child: Center(
-                                    //       child: Container(
-                                    //         padding:
-                                    //             EdgeInsets.fromLTRB(5, 0, 0, 0),
-                                    //         child: Center(
-                                    //             child: Text("Makanan Ringan")),
-                                    //         color: Colors.blueAccent,
-                                    //       ),
-                                    //     )),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
+      body: buildbody(foodList),
+    );
+  }
+
+  SafeArea buildbody(List<String> foodList) {
+    return SafeArea(
+      child: Column(
+        children: [
+          buildSearchRow(),
+          buildColumnProduct(foodList),
+          buildPopularRow(),
+          buildPopularList(foodList),
+        ],
+      ),
+    );
+  }
+
+  SizedBox buildPopularList(List<String> foodList) {
+    return SizedBox(
+      height: 200,
+      child: ListView.builder(
+        itemCount: foodList.length,
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          return buildListItem(context);
+        },
+      ),
+    );
+  }
+
+  Container buildListItem(BuildContext context) {
+    return Container(
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Row(
+          children: <Widget>[
+            buildPopularListImage(context),
+            buildPopularListText(context),
           ],
         ),
+      ),
+    );
+  }
+
+  Expanded buildPopularListText(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(5, 0, 10, 0),
+        child: SizedBox(
+            height: MediaQuery.of(context).size.width * 0.15,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                buildPopularTextTitle(),
+                Spacer(
+                  flex: 1,
+                ),
+                Row(
+                  children: [
+                    buildPopularTag("Gorengan", Colors.orange),
+                    buildPopularTag("Makinan Ringan", Colors.blue),
+                  ],
+                ),
+                Spacer(
+                  flex: 2,
+                ),
+                buildPopularInfo()
+              ],
+            )),
+      ),
+    );
+  }
+
+  Row buildPopularInfo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          "Warung buyakuk",
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.black26, fontSize: 12),
+        ),
+        Text(
+          "Rp. 2.500",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Container buildPopularTag(String txt, Color tagColor) {
+    return Container(
+      child: Text(
+        txt,
+        style: TextStyle(color: tagColor, fontSize: 12),
+      ),
+      //
+      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+      margin: EdgeInsets.only(right: 5),
+      decoration: BoxDecoration(
+        color: tagColor.withOpacity(0.10),
+        borderRadius: BorderRadius.circular(5),
+      ),
+    );
+  }
+
+  Text buildPopularTextTitle() {
+    return Text(
+      "Banana Goreng",
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 16,
+      ),
+    );
+  }
+
+  Card buildPopularListImage(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: Image.network(
+        "https://c.stocksy.com/a/RMl000/z9/182059.jpg",
+        width: MediaQuery.of(context).size.width * 0.17,
+        height: MediaQuery.of(context).size.width * 0.17,
+        fit: BoxFit.cover,
       ),
     );
   }
@@ -141,64 +150,75 @@ class _HomeViewState extends State<HomeView> {
       height: 80,
       child: Row(
         children: [
-          Container(
-            height: 60,
-            width: 60,
-            padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
-            child: FittedBox(
-              child: FloatingActionButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                child: Icon(
-                  Icons.favorite,
-                  color: Colors.white,
-                ),
-                onPressed: null,
-                backgroundColor: Colors.redAccent,
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Popular",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-                Row(
-                  children: [Text("Monggo, entekno duwekmu!")],
-                ),
-              ],
-            ),
-          ),
+          buildHearthButton(),
+          buildPopularRowText(),
           Spacer(),
-          Container(
-            height: 60,
-            width: 60,
-            padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
-            child: FittedBox(
-              child: FloatingActionButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                child: Icon(
-                  Icons.arrow_right,
-                  color: Colors.black,
-                ),
-                onPressed: null,
-                backgroundColor: Colors.white,
-              ),
-            ),
+          buildRightArrowButton(),
+        ],
+      ),
+    );
+  }
+
+  Container buildRightArrowButton() {
+    return Container(
+      height: 60,
+      width: 60,
+      padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+      child: FittedBox(
+        child: FloatingActionButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: Icon(
+            Icons.arrow_right,
+            color: Colors.black,
+          ),
+          onPressed: null,
+          backgroundColor: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  Container buildPopularRowText() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Popular",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Row(
+            children: [Text("Monggo, entekno duwekmu!")],
           ),
         ],
+      ),
+    );
+  }
+
+  Container buildHearthButton() {
+    return Container(
+      height: 60,
+      width: 60,
+      padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+      child: FittedBox(
+        child: FloatingActionButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: Icon(
+            Icons.favorite,
+            color: Colors.white,
+          ),
+          onPressed: null,
+          backgroundColor: Colors.redAccent,
+        ),
       ),
     );
   }
@@ -257,36 +277,41 @@ class _HomeViewState extends State<HomeView> {
   Card cListCard(List<String> categoryList, int index) {
     return Card(
       child: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Image.network(
-              "https://media.istockphoto.com/photos/donut-with-sprinkles-isolated-picture-id538335769",
-              fit: BoxFit.cover,
-              height: 140,
-              width: 140,
-              alignment: Alignment.center,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                padding: EdgeInsets.fromLTRB(0, 2, 2, 0),
-                child: Text("Sweets -"),
-              ),
-              Container(
-                  padding: EdgeInsets.fromLTRB(2, 2, 0, 0),
-                  child: Text(
-                    "12 Item",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ))
-            ],
-          )
-        ],
+        children: <Widget>[buildCListImage(), buildCListText()],
+      ),
+    );
+  }
+
+  Row buildCListText() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          padding: EdgeInsets.fromLTRB(0, 2, 2, 0),
+          child: Text("Sweets -"),
+        ),
+        Container(
+            padding: EdgeInsets.fromLTRB(2, 2, 0, 0),
+            child: Text(
+              "12 Item",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ))
+      ],
+    );
+  }
+
+  Container buildCListImage() {
+    return Container(
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Image.network(
+        "https://media.istockphoto.com/photos/donut-with-sprinkles-isolated-picture-id538335769",
+        fit: BoxFit.cover,
+        height: 140,
+        width: 140,
+        alignment: Alignment.center,
       ),
     );
   }
@@ -300,25 +325,33 @@ class _HomeViewState extends State<HomeView> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.arrow_back_ios,
-                color: Colors.black,
-              ),
-              onPressed: null,
-            ),
-            Transform.rotate(
-              angle: 180 * math.pi / 180,
-              child: IconButton(
-                icon: Icon(
-                  Icons.short_text,
-                  color: Colors.black,
-                ),
-                onPressed: null,
-              ),
-            ),
+            buildBackButton(),
+            buildCategoryButton(),
           ],
         ),
+      ),
+    );
+  }
+
+  IconButton buildBackButton() {
+    return IconButton(
+      icon: Icon(
+        Icons.arrow_back_ios,
+        color: Colors.black,
+      ),
+      onPressed: null,
+    );
+  }
+
+  Transform buildCategoryButton() {
+    return Transform.rotate(
+      angle: 180 * math.pi / 180,
+      child: IconButton(
+        icon: Icon(
+          Icons.short_text,
+          color: Colors.black,
+        ),
+        onPressed: null,
       ),
     );
   }
